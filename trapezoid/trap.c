80c1542e047ae517303826f7787f12cbac752152
SIGMA1425 <yahasho1425@gmail.com>
2020-01-21 10:26:11 +0900

    trap_sinpleを関数ポインタに対応させた

--- trapezoid/trap.c
+++ trapezoid/trap.c
@@ -2,14 +2,16 @@
 #include <math.h>
 
 double x_2(double);
-double trap_sinple(double, double);
+
+double (*funcp)(double);
+double trap_sinple(double, double, double (*funcp)(double));
 
 int main(void)
 {
     double a, b;
     double s;
     scanf("%lf %lf", &a, &b);
-    s = trap_sinple(a, b);
+    s = trap_sinple(a, b, sin);
     printf("%lf\n", s);
     return 0;
 }
@@ -19,9 +21,9 @@ double x_2(double x)
     return x*x;
 }
 
-double trap_sinple(double a, double b)
+double trap_sinple(double a, double b, double (*funcp)(double))
 {
     double s;
-    s = ((b-a)/2) * (x_2(a) + x_2(b));
+    s = ((b-a)/2) * ((*funcp)(a) + (*funcp)(b));
     return s;
 }
