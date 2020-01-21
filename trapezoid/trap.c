70243548774494e4a536b965804840e49c1d59af
SIGMA1425 <yahasho1425@gmail.com>
2020-01-21 21:19:35 +0900

    台形則の合成積分を関数(trap_com)にした

--- trapezoid/trap.c
+++ trapezoid/trap.c
@@ -2,29 +2,12 @@
 #include <math.h>
 
 double x_2(double);
-double (*funcp)(double);
 double trap_sinple(double, double, double (*funcp)(double));
+double trap_com(double, double, int, double (*funcp)(double));
 
 int main(void)
 {
-    double a, b;    //積分範囲
-    double s = 0;   //答え
-    double h;       //一分割の幅
-    int n;          //分割数
-    int i;
-
-    scanf("%lf %lf %d", &a, &b, &n);
-
-    h = (b-a) / (n-1);
-
-    s += x_2(a);
-    for(i=2; i<n; i++)
-    {
-        s += 2 * x_2(a + h * (i-1));
-    }
-    s += x_2(b);
-    s = s * h / 2;
-    
+    double s = trap_com(0, M_PI, 10, sin);
     printf("%lf\n", s);
     return 0;
 }
@@ -38,5 +21,21 @@ double trap_sinple(double a, double b, double (*funcp)(double))
 {
     double s;
     s = ((b-a)/2) * ((*funcp)(a) + (*funcp)(b));
+    return s;
+}
+
+double trap_com(double a, double b, int n, double (*funcp)(double))
+{
+    double s = 0;               //答え
+    double h = (b-a) / (n-1);   //分割幅
+    int i;
+
+    s += (*funcp)(a);
+    for(i=2; i<n; i++)
+    {
+        s += 2 * (*funcp)(a + h * (i-1));
+    }
+    s += (*funcp)(b);
+    s = s * h / 2;
     return s;
 }
