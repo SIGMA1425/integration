4c46917ae500658c5d18c72cc165ca43d08c54fe
SIGMA1425 <yahasho1425@gmail.com>
2020-01-21 21:06:44 +0900

    データをn分割して台形則をするプログラムをmainに書いた

--- trapezoid/trap.c
+++ trapezoid/trap.c
@@ -2,16 +2,29 @@
 #include <math.h>
 
 double x_2(double);
-
 double (*funcp)(double);
 double trap_sinple(double, double, double (*funcp)(double));
 
 int main(void)
 {
-    double a, b;
-    double s;
-    scanf("%lf %lf", &a, &b);
-    s = trap_sinple(a, b, sin);
+    double a, b;    //積分範囲
+    double s = 0;   //答え
+    double h;       //一分割の幅
+    int n;          //分割数
+    int i;
+
+    scanf("%lf %lf %d", &a, &b, &n);
+
+    h = (b-a) / (n-1);
+
+    s += x_2(a);
+    for(i=2; i<n; i++)
+    {
+        s += 2 * x_2(a + h * (i-1));
+    }
+    s += x_2(b);
+    s = s * h / 2;
+    
     printf("%lf\n", s);
     return 0;
 }
